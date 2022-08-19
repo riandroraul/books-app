e309b5f105041ea4b8626f736f58de4062bb8320
riandroraul <riandroraul@gmail.com>
2022-08-19 16:38:37 +0700

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Navbar from "./navbar";
+
+function Home() {
+  const title = "Halaman Home";
+  return (
+    <div>
+      <Navbar />
+      <div className="container">
+        <h1>{title}</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. A,
+          odio?Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Accusamus tempore officiis ex distinctio aut sint culpa, voluptatibus
+          reiciendis, temporibus neque consequatur perspiciatis ducimus
+          veritatis consequuntur ipsum, aspernatur nemo at veniam.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
